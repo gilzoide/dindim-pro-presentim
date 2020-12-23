234f998322ae45ae231dc70aca2ec79083fd09e1
gilzoide <gilzoide@gmail.com>
2020-12-23 19:44:49 -0300

    Add molinha 2D e script de movimentação

---
+++ Player/Player.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+export var linearVelocity: float = 100
+var direction = Vector2.ZERO
+
+onready var animationPlayer: AnimationPlayer = $AnimationPlayer
+
+func _ready():
+	set_physics_process(true)
+
+func _physics_process(delta):
+	if Input.is_action_pressed("move_up"):
+		direction.y = -1
+	elif Input.is_action_pressed("move_down"):
+		direction.y = 1
+	else:
+		direction.y = 0
+	
+	if Input.is_action_pressed("move_left"):
+		direction.x = -1
+	elif Input.is_action_pressed("move_right"):
+		direction.x = 1
+	else:
+		direction.x = 0
+	if direction.x != 0 or direction.y != 0:
+		direction = direction.normalized()
+		animationPlayer.play("PlayerAnimation")
+	move_and_collide(direction * linearVelocity * delta)
