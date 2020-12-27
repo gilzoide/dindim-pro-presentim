8bc80be5149f631360edb4c7fb4b597c012f690f
gilzoide <gilzoide@gmail.com>
2020-12-26 23:22:35 -0300

    WIP Add Lazy Mosquito

---
+++ Inimigos/LazyMosquito.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+
+export(Vector2) var direction: Vector2 = Vector2(1, 1)
+export(float) var speed = 200
+onready var sprite: AnimatedSprite = $AnimatedSprite
+
+func _ready():
+	set_physics_process(true)
+	sprite.play()
+
+func _physics_process(delta):
+	var collision = move_and_collide(direction * speed * delta)
+	if collision:
+		direction = direction.bounce(collision.normal)
+		sprite.flip_h = direction.x < 0
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Jogo de amigo oculto, tomara que a pessoa goste!
 ## Créditos
 - Dinomouse font by fontgrove: https://www.fontspace.com/dinomouse-font-f44061
 - Artic Landscape art by Delfos: https://opengameart.org/content/artic-landscape-background
+- Lazy Mosquito art by bevouliin: https://opengameart.org/content/lazy-mosquito
