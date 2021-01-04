4f0e0c74a595274af58be185ee8c8583e0da4371
gilzoide <gilzoide@gmail.com>
2021-01-04 20:48:12 -0300

    Amend Fix do problema de CCD com googly eyes

--- GooglyEye/Pupila.gd
+++ GooglyEye/Pupila.gd
@@ -3,11 +3,13 @@ extends RigidBody2D
 onready var parent = get_node("..")
 onready var radius = get_node("../Fundo").texture.get_width() * 0.5
 onready var squared_radius = radius * radius
+onready var self_radius = get_node("Sprite").texture.get_width() * 0.5
+onready var squared_self_radius = self_radius * self_radius
 
 func _integrate_forces(state):
 	var origin : Vector2 = state.transform.origin
 	var delta : Vector2 = origin - parent.global_position
-	var squared_length = delta.length_squared()
+	var squared_length = delta.length_squared() - squared_self_radius
 	if squared_length > squared_radius:
 		delta *= radius / sqrt(squared_length)
 		state.transform.origin -= delta
