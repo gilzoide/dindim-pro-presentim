229f37fcf022c7ab7139442cc8c858a1918c996b
gilzoide <gilzoide@gmail.com>
2021-01-04 20:59:23 -0300

    Fix velocidades extremas dos googly eyes

--- GooglyEye/Pupila.gd
+++ GooglyEye/Pupila.gd
@@ -1,5 +1,7 @@
 extends RigidBody2D
 
+export(float) var clamp_speed_on_too_far = 500
+
 onready var parent = get_node("..")
 onready var radius = get_node("../Fundo").texture.get_width() * 0.5
 onready var squared_radius = radius * radius
@@ -11,5 +13,7 @@ func _integrate_forces(state):
 	var delta : Vector2 = origin - parent.global_position
 	var squared_length = delta.length_squared() - squared_self_radius
 	if squared_length > squared_radius:
+		if squared_length > squared_radius * 2:
+			linear_velocity = linear_velocity.clamped(clamp_speed_on_too_far)
 		delta *= radius / sqrt(squared_length)
 		state.transform.origin -= delta
